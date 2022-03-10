a94b6ea77ad2d0b44e6b7b46e4adda75ae83c82d
Monnoroch <sidelnikov.ns@gmail.com>
2022-03-10 23:03:01 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/viper"
+)
+
+type config struct{
+	Port string `json:"port"`
+	ConnectionString string `json:"ConnectionString"`
+}
+
+type test interface{}
+
+func ReadConfig() (*config, error){
+	var C config
+	var T test
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = viper.Unmarshal(&T)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(T)
+	return &C, nil
+
+}
+
+func Process(w http.ResponseWriter, r *http.Request){
+
+}
+
+func main(){
+
+	conf, err := ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(conf)
+	http.HandleFunc("/", Process)
+	http.ListenAndServe(":8082", nil)
+}
